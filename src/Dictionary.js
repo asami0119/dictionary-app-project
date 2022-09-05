dda017a289858dd047933c830a844fb4590e4762
asami0119 <assa.0119.0425.0905@gmail.com>
2022-09-05 00:17:32 -0400

    Added dictionary axios integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function showResult(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for the definition of ${keyword}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+
+    axios.get(apiUrl).then(showResult);
   }
   function handleChange(event) {
     console.log(event.target.value);
