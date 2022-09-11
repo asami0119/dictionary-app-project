8c0fa5cab5b510c0b581ac57c83cb55392c7014b
asami0119 <assa.0119.0425.0905@gmail.com>
2022-09-11 09:22:08 -0400

    Update Example.js

--- src/Example.js
+++ src/Example.js
@@ -1,6 +1,18 @@
+import React from "react";
+
 export default function Example(props) {
-  if (props.example) {
-    return "hello";
+  if (props.example && props.example.length > 0) {
+    return (
+      <div className="Example">
+        {props.examples.map(function (example, index) {
+          return (
+            <ul key={index}>
+              <li>{example}</li>
+            </ul>
+          );
+        })}
+      </div>
+    );
   } else {
     return null;
   }
