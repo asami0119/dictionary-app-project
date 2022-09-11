9dc1073be45dff6277e70cc480bf9ff6cc7662e1
asami0119 <assa.0119.0425.0905@gmail.com>
2022-09-11 00:14:15 -0400

    updated "Phonetic" component

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -6,8 +6,11 @@ export default function Phonetic(props) {
   return (
     <div>
       <div className="ms-2">{props.phonetic.text}</div>
-      <a href={props.phonetic.audio} target="_blank">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPtPlbIfGNktsqsvBrfm-9neNVY5zoNtt2Xw&usqp=CAU" />
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPtPlbIfGNktsqsvBrfm-9neNVY5zoNtt2Xw&usqp=CAU"
+          alt="speaker-icon"
+        />
       </a>
     </div>
   );
